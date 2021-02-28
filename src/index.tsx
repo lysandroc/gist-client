c5202480f44d14c8c23cc5f786625770d36a467b
Lysandro Carioca <lysandroc@gmail.com>
2021-02-28 19:48:17 -0300

    Removed react query to fix compile issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { GlobalStyle } from 'theme/globalStyle';
 import App from './App';
-
-const queryClient = new QueryClient();
 
 ReactDOM.render(
     <React.StrictMode>
         <GlobalStyle />
-        <QueryClientProvider client={queryClient}>
-            <App />
-        </QueryClientProvider>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
